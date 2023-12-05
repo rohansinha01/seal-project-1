1a3917ace858d76563dc4c67a076a2fa91a92874
rohansinha01 <rohansinha01@gmail.com>
2023-12-05 00:26:49 -0500

    console.logged a function to get a searched function

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+function getQuote(quote) {
+
+
+
 const randomurl = 'https://api.quotable.io/quotes/random'
+const searchedurl = `https://api.quotable.io/search/quotes?query=${quote}`
+
 
 fetch(randomurl)
 .then((res) => res.json())
 .then((data) => console.log(data))
 
-const searchedurl = 'https://api.quotable.io/search/quotes?query=life+happiness'
+
 
 fetch(searchedurl)
 .then((res) => res.json())
 .then((search) => console.log(search))
+}
+function renderQuote(quote) {
+    const quotediv = document.querySelector('.quote')
+    quotediv.innerHTML = 
+    '<h1>quotediv</h1>'
+}
+
+
+
+getQuote('happiness')
+
