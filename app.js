78c22c82c1a42053de7b7aa738f214629722791c
rohansinha01 <rohansinha01@gmail.com>
2023-12-05 23:46:28 -0500

    got searched JSON to show up on screen

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ const searchedurl = `https://api.quotable.io/search/quotes?query=${quote}`
 
 fetch(searchedurl)
 .then((res) => res.json())
-.then((search) => console.log(search))
+.then((search) => renderSearch(search))
 }
 
-//Function to get a random quote
+//Get a searched quote to be on the page
+
+function renderSearch(search) {
+    const searchdiv = document.querySelector('.search')
+    const fullQuote = JSON.stringify(search.results)
+    
+    searchdiv.innerHTML =`<h1>${fullQuote}</h1>`
+    } 
+
+// Funciton to handle the search function
+
+// Function to get a random quote
 function randomQuote() {
 const randomurl = 'https://api.quotable.io/quotes/random'   
 
@@ -23,11 +34,22 @@ function renderRandom(random) {
     const quotediv = document.querySelector('.random')
     quotediv.innerHTML = `<h1>${random[0].content}</h1>`
 }
+// function to handle the form submission
+function handleSubmit(event){
+    // prevent the refreshing of the page from the form submission
+    event.preventDefault()
+    // grab the form from the event
+    const form = event.target
+    console.log(form)
 
-//Use jQuery to get the random button to work
-function showRandom() {
-    document.querySelectorAll('button')
+    const formData = new FormData(form)
+    // grab the search
+    const quote = formData.get("quote")
+    console.log(quote)
+
+    searchQuote(quote)
 }
-
 //Main Functions
+document.querySelector('form').addEventListener('submit', handleSubmit)
+searchQuote()
 randomQuote()
